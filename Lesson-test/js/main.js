625e0046e60c5d6ad00bce0143472aeb2b9c0620
OBIDJ0N <101195736+OBIDJ0N@users.noreply.github.com>
2022-07-29 07:29:04 +0500

    create responsivibility

---
+++ Lesson-test/js/main.js
@@ -0,0 +1,31 @@
+$('.slider-blog__inner').slick(
+    {
+        dots:true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/portfolio/left.svg" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/portfolio/right.svg" alt="right"></button>',
+    }
+);
+
+$(function(){
+    $('.slider-blog__inner').slick(
+        {
+        dots:true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/portfolio/left.svg" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/portfolio/right.svg" alt="right"></button>',
+        
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+var mixer = mixitup('.portfolio__content');
